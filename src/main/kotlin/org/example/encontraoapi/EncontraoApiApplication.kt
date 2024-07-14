11e2ccbae3585a4bcb486ac7dd39df627fb82e91
Matheus Cogo <matheuscogo@wswork.com.br>
2024-07-14 19:16:03 -0300

    Initial commit

---
+++ src/main/kotlin/org/example/encontraoapi/EncontraoApiApplication.kt
@@ -0,0 +1,11 @@
+package org.example.encontraoapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EncontraoApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<EncontraoApiApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/encontraoapi/EncontraoApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.encontraoapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EncontraoApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
