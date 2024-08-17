5f3c5daf645468ba2b132d37be3a46a31d4690c2
Matheus Cogo <matheuscogo@wswork.com.br>
2024-08-16 23:37:50 -0300

    change sql

--- src/main/resources/migrations/tables.sql
+++ src/main/resources/migrations/tables.sql
@@ -463,89 +463,91 @@ DROP TABLE IF EXISTS teams CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
 
 
+
 -- Tabela presentations
-INSERT INTO
-    presentations ("order", entrance_choreography, traditional_dance_1, traditional_dance_2,
-                   traditional_dance_3, exit_choreography, birivas_dances_1, birivas_dances_2,
-                   birivas_dances_3, deleted, created, created_by, updated, updated_by)
-VALUES
-    (1, 'Entrance 1', 'Dance 1A', 'Dance 2A', 'Dance 3A', 'Exit 1A', 'Birivas Dance 1A', 'Birivas Dance 2A', 'Birivas Dance 3A', FALSE, NOW(), 1, NULL, NULL),
-    (2, 'Entrance 2', 'Dance 1B', 'Dance 2B', 'Dance 3B', 'Exit 2B', 'Birivas Dance 1B', 'Birivas Dance 2B', 'Birivas Dance 3B', FALSE, NOW(), 2, NULL, NULL),
-    (3, 'Entrance 3', 'Dance 1C', 'Dance 2C', 'Dance 3C', 'Exit 3C', 'Birivas Dance 1C', 'Birivas Dance 2C', 'Birivas Dance 3C', FALSE, NOW(), 3, NULL, NULL);
+INSERT INTO presentations (
+    "order", "entrance_choreography", "traditional_dance_1", "traditional_dance_2",
+    "traditional_dance_3", "exit_choreography", "birivas_dances_1", "birivas_dances_2",
+    "birivas_dances_3", "deleted", "created", "created_by", "updated", "updated_by"
+) VALUES
+    (0, 'GiraGira na vitrola', 'Anú', 'Tatu de castanholas', '', '', '', '', '', FALSE, NOW(), 3, NULL, NULL);
 
 -- Tabela campus
-INSERT INTO
-    campus (institution, coordinator_name, description, entity, dormitory, id_presentation,
-            deleted, created, created_by, updated, updated_by)
-VALUES
-    ('Institution A', 'Coordinator A', 'Description A', 'Entity A', 'Dormitory A', 1, FALSE, NOW(), 1, NULL, NULL),
-    ('Institution B', 'Coordinator B', 'Description B', 'Entity B', 'Dormitory B', 2, FALSE, NOW(), 2, NULL, NULL),
-    ('Institution C', 'Coordinator C', 'Description C', 'Entity C', 'Dormitory C', 3, FALSE, NOW(), 3, NULL, NULL);
+INSERT INTO campus (
+    "institution", "coordinator_name", "description", "entity", "dormitory", "id_presentation",
+    "deleted", "created", "created_by", "updated", "updated_by"
+) VALUES
+    ('IFFar - SVS', 'Eduardo Feitoza', 'Instituto Federal Farroupilha - Campus São Vicente do Sul', 'NTG Trempe da Saudade', 'Predio B', 1, FALSE, NOW(), 1, NULL, NULL);
 
 -- Tabela users
-INSERT INTO
-    users (name, cpf, registration, password, roles, is_evaluator, is_admin,
-           id_campus, deleted, created, created_by, updated, updated_by)
-VALUES
-    ('User A', '12345678901', 'Reg123', 'Password123', 'RoleA', FALSE, FALSE, 1, FALSE, NOW(), 1, NULL, NULL),
-    ('User B', '23456789012', 'Reg456', 'Password456', 'RoleB', TRUE, FALSE, 2, FALSE, NOW(), 2, NULL, NULL),
-    ('User C', '34567890123', 'Reg789', 'Password789', 'RoleC', FALSE, TRUE, 3, FALSE, NOW(), 3, NULL, NULL);
+INSERT INTO users (
+    "name", "cpf", "registration", "password", "roles", "is_evaluator", "is_admin",
+    "id_campus", "deleted", "created", "created_by", "updated", "updated_by"
+) VALUES
+    ('Mauricio Carvalho Cogo', '05415375040', '2023001236', '05415375040', 'Estudante', FALSE, TRUE, 1, FALSE, NOW(), 1, NULL, NULL);
 
 -- Tabela points
-INSERT INTO
-    points (name, type, descption, icon, latitude, longitude,
-            deleted, created, created_by, updated, updated_by)
-VALUES
-    ('Point A', 'Type A', 'Description A', 'Icon A', 'Lat A', 'Lon A', FALSE, NOW(), 1, NULL, NULL),
-    ('Point B', 'Type B', 'Description B', 'Icon B', 'Lat B', 'Lon B', FALSE, NOW(), 2, NULL, NULL),
-    ('Point C', 'Type C', 'Description C', 'Icon C', 'Lat C', 'Lon C', FALSE, NOW(), 3, NULL, NULL);
-
+INSERT INTO points (
+    "name", "type", "description", "icon", "latitude", "longitude",
+    "deleted", "created", "created_by", "updated", "updated_by"
+) VALUES
+      ('Dança de salão', 'Prova', 'Forme uma dupla, dance duas das seguintes danças: Chote, Milonga, Chamamé, Rancheira, Valsa, Marzuca, ou Bugio, em dois minutos cada, e conquiste a maior pontuação para vencer!', '---', '-29.70438062047002', '-54.69807524954617', FALSE, NOW(), 1, NULL, NULL),
+      ('Revezamento do mate', 'Prova', 'Forme uma equipe de quatro (dois peões e duas prendas), ceve, tome e passe o mate em revezamento. A equipe que completar a prova no menor tempo vence!', '---', '-29.704254157352864', '-54.69765847554981', FALSE, NOW(), 1, NULL, NULL),
+      ('Triatlo campeiro', 'Prova', 'Forme uma equipe de seis (três peões e três prendas) e complete as três etapas: nó de lenço, corrida do saco e debulhar milho. A equipe que finalizar todas as etapas primeiro vence!', '---', '-29.704254157352864', '-54.69765847554981', FALSE, NOW(), 1, NULL, NULL),
+      ('Tetarfe', 'Prova', 'Participe em duplas (um peão e uma prenda) e acumule pontos em lançamentos seguindo o Regulamento de Esportes Campeiros do MTG. Vence a equipe com a maior pontuação total. Em caso de empate, será considerado o maior desempenho em argola, ferradura e tava, nessa ordem.', '---', '-29.702772406805014', '-54.69581553656609', FALSE, NOW(), 1, NULL, NULL),
+      ('Tiro de laço vaca parada', 'Prova', 'Forme uma dupla, laçe as aspas da vaca parada a 8 metros (homens) ou 2 metros (mulheres) em três tentativas, e vença acertando mais laçadas válidas.', '---', '-29.704254157352864', '-54.69765847554981', FALSE, NOW(), 1, NULL, NULL),
+      ('Pescaria gaúcha', 'Prova', 'Forme uma dupla com um peão e uma prenda, monte sua vara de pescar, busque sua isca e pesque o peixe mais pesado para vencer!', '---', '-29.705887946838324', '-54.696781999287786', FALSE, NOW(), 1, NULL, NULL),
+      ('Truco', 'Prova', 'Forme uma equipe de três e participe das partidas eliminatórias de Truco Gaudério. A competição segue o Regulamento de Esportes Campeiros do MTG, com formato “melhor de três partidas”. A equipe vencedora será a campeã!', '---', '-29.70438062047002', '-54.69807524954617', FALSE, NOW(), 1, NULL, NULL),
+      ('Arroz Carreteiro', 'Prova', 'Forme uma equipe (um peão e uma prenda) e prepare arroz carreteiro para 40 pessoas. Os ingredientes e utensílios serão fornecidos, mas os temperos são de sua escolha para destacar sua identidade regional.', '---', '-29.70438062047002', '-54.69807524954617', FALSE, NOW(), 1, NULL, NULL),
+      ('Artilharia gaúcha', 'Prova', 'Forme uma equipe de quatro (dois peões e duas prendas) e arremesse pedras com um bodoque para atingir os alvos. Cada acerto vale um ponto e a equipe com mais pontos vence. Em caso de empate, cada participante terá mais um disparo. Traga seu bodoque tradicional!', '---', '-29.702380714802718', '-54.69506985330737', FALSE, NOW(), 1, NULL, NULL),
+      ('Cabo de guerra', 'Prova', 'Forme uma equipe de sete (mínimo de duas prendas) e participe do cabo de guerra com uma corda de 14 metros. Puxe a corda para seu lado e faça com que pelo menos um adversário ultrapasse a marca no chão. A equipe vencedora será a campeã!', '---', '-29.702380714802718', '-54.69506985330737', FALSE, NOW(), 1, NULL, NULL),
+      ('Bocha campeira', 'Prova', 'Forme uma equipe de três (dois peões e uma prenda) e jogue bocha campeira seguindo o Regulamento de Esportes Campeiros do MTG. As partidas são disputadas em duas passadas, com uma passada extra em caso de empate. A equipe com mais pontos vence!', '---', '-29.702380714802718', '-54.69506985330737', FALSE, NOW(), 1, NULL, NULL);
 -- Tabela competitions
-INSERT INTO
-    competitions (name, modality, descption, fecult_event, participants, commission, id_point,
-                  deleted, created, created_by, updated, updated_by)
-VALUES
-    ('Competition A', 'Modality A', 'Description A', FALSE, 10, FALSE, 1, FALSE, NOW(), 1, NULL, NULL),
-    ('Competition B', 'Modality B', 'Description B', TRUE, 20, TRUE, 2, FALSE, NOW(), 2, NULL, NULL),
-    ('Competition C', 'Modality C', 'Description C', FALSE, 30, FALSE, 3, FALSE, NOW(), 3, NULL, NULL);
+INSERT INTO competitions (
+    "name", "modality", "descption", "fecult_event", "participants", "commission", "id_point",
+    "deleted", "created", "created_by", "updated", "updated_by"
+) VALUES
+      ('Competition A', 'Modality A', 'Description A', FALSE, 10, FALSE, 1, FALSE, NOW(), 1, NULL, NULL),
+      ('Competition B', 'Modality B', 'Description B', TRUE, 20, TRUE, 2, FALSE, NOW(), 2, NULL, NULL),
+      ('Competition C', 'Modality C', 'Description C', FALSE, 30, FALSE, 3, FALSE, NOW(), 3, NULL, NULL);
 
 -- Tabela teams
-INSERT INTO
-    teams (grade, deleted, created, created_by, updated, updated_by)
-VALUES
-    (95.5, FALSE, NOW(), 1, NULL, NULL),
-    (88.0, FALSE, NOW(), 2, NULL, NULL),
-    (92.3, FALSE, NOW(), 3, NULL, NULL);
+INSERT INTO teams (
+    "grade", "deleted", "created", "created_by", "updated", "updated_by"
+) VALUES
+      (95.5, FALSE, NOW(), 1, NULL, NULL),
+      (88.0, FALSE, NOW(), 2, NULL, NULL),
+      (92.3, FALSE, NOW(), 3, NULL, NULL);
 
 -- Tabela teams_users
-INSERT INTO
-    teams_users (id_user, id_team, deleted, created, created_by, updated, updated_by)
-VALUES
-    (1, 1, FALSE, NOW(), 1, NULL, NULL),
-    (2, 2, FALSE, NOW(), 2, NULL, NULL),
-    (3, 3, FALSE, NOW(), 3, NULL, NULL);
+INSERT INTO teams_users (
+    "id_user", "id_team", "deleted", "created", "created_by", "updated", "updated_by"
+) VALUES
+      (1, 1, FALSE, NOW(), 1, NULL, NULL),
+      (2, 2, FALSE, NOW(), 2, NULL, NULL),
+      (3, 3, FALSE, NOW(), 3, NULL, NULL);
 
 -- Tabela competitions_teams
-INSERT INTO
-    competitions_teams (id_team, id_competition, deleted, created, created_by, updated, updated_by)
-VALUES
-    (1, 1, FALSE, NOW(), 1, NULL, NULL),
-    (2, 2, FALSE, NOW(), 2, NULL, NULL),
-    (3, 3, FALSE, NOW(), 3, NULL, NULL);
+INSERT INTO competitions_teams (
+    "id_team", "id_competition", "deleted", "created", "created_by", "updated", "updated_by"
+) VALUES
+      (1, 1, FALSE, NOW(), 1, NULL, NULL),
+      (2, 2, FALSE, NOW(), 2, NULL, NULL),
+      (3, 3, FALSE, NOW(), 3, NULL, NULL);
 
 -- Tabela commission
-INSERT INTO
-    commission (id_competitions_teams, id_user, grade_1, grade_2, grade_3, grade_4, grade_5,
-                deleted, created, created_by, updated, updated_by)
-VALUES
-    (1, 1, 90.0, 85.0, 80.0, 75.0, 70.0, FALSE, NOW(), 1, NULL, NULL),
-    (2, 2, 85.0, 80.0, 75.0, 70.0, 65.0, FALSE, NOW(), 2, NULL, NULL),
-    (3, 3, 80.0, 75.0, 70.0, 65.0, 60.0, FALSE, NOW(), 3, NULL, NULL);
+INSERT INTO commission (
+    "id_competitions_teams", "id_user", "grade_1", "grade_2", "grade_3", "grade_4", "grade_5",
+    "deleted", "created", "created_by", "updated", "updated_by"
+) VALUES
+      (1, 1, 90.0, 85.0, 80.0, 75.0, 70.0, FALSE, NOW(), 1, NULL, NULL),
+      (2, 2, 85.0, 80.0, 75.0, 70.0, 65.0, FALSE, NOW(), 2, NULL, NULL),
+      (3, 3, 80.0, 75.0, 70.0, 65.0, 60.0, FALSE, NOW(), 3, NULL, NULL);
 
 -- Tabela timeline
-INSERT INTO
-    timeline (name, required, date, id_point, id_competition, deleted, created, created_by, updated, updated_by)
-VALUES
-    ('Timeline Event A', FALSE, '2024-07-24 00:00:00', 1, 1, FALSE, NOW(), 1, NULL, NULL),
-    ('Timeline Event B', TRUE, '2024-07-25 00:00:00', 2, 2, FALSE, NOW(), 2, NULL, NULL),
-    ('Timeline Event C', FALSE, '2024-07-26 00:00:00', 3, 3, FALSE, NOW(), 3, NULL, NULL);
+INSERT INTO timeline (
+    "name", "required", "date", "id_point", "id_competition", "deleted", "created", "created_by", "updated", "updated_by"
+) VALUES
+      ('Timeline Event A', FALSE, '2024-07-24 00:00:00', 1, 1, FALSE, NOW(), 1, NULL, NULL),
+      ('Timeline Event B', TRUE, '2024-07-25 00:00:00', 2, 2, FALSE, NOW(), 2, NULL, NULL),
+      ('Timeline Event C', FALSE, '2024-07-26 00:00:00', 3, 3, FALSE, NOW(), 3, NULL, NULL);
