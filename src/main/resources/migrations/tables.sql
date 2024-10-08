8a260441d0ece9177a28b9bc915b22efa8f67e09
Mauricio C Cogo <Mauricioccogo@gmail.com>
2024-10-08 14:39:03 -0300

    Adicionando o resto

--- src/main/resources/migrations/tables.sql
+++ src/main/resources/migrations/tables.sql
@@ -23,11 +23,11 @@ CREATE TABLE IF NOT EXISTS presentations
 (
     id                    BIGSERIAL PRIMARY KEY,
     "order"               INTEGER   DEFAULT NULL,
+    "date"				  TIMESTAMP NOT NULL,
     entrance_choreography VARCHAR   NULL,
     traditional_dance_1   VARCHAR   NOT NULL,
     traditional_dance_2   VARCHAR   DEFAULT NULL,
     traditional_dance_3   VARCHAR   DEFAULT NULL,
-    traditional_dance_extra   VARCHAR   DEFAULT NULL,
     exit_choreography     VARCHAR   NULL,
     birivas_dances_1      VARCHAR   NULL,
     birivas_dances_2      VARCHAR   NULL,
@@ -86,6 +86,7 @@ CREATE TABLE IF NOT EXISTS points
     type       VARCHAR   NOT NULL,
     description  VARCHAR   NOT NULL,
     icon       VARCHAR   NOT NULL,
+    size 	   INTEGER	 NOT NULL,
     latitude   VARCHAR   NOT NULL,
     longitude  VARCHAR   NOT NULL,
     deleted    BOOLEAN   DEFAULT FALSE,
@@ -183,6 +184,7 @@ CREATE TABLE IF NOT EXISTS timeline
     name           VARCHAR   NOT NULL,
     required       BOOLEAN   DEFAULT FALSE,
     date           TIMESTAMP NOT NULL,
+    type           VARCHAR   NOT NULL,  -- Adicionada a nova coluna aqui
     id_point       BIGINT    NOT NULL,
     id_competition BIGINT    NULL,
     deleted        BOOLEAN   DEFAULT FALSE,
@@ -195,81 +197,17 @@ CREATE TABLE IF NOT EXISTS timeline
     );
 
 
+ALTER SEQUENCE public.campus_id_seq RESTART WITH 1;
+ALTER SEQUENCE public.commissions_id_seq RESTART WITH 1;
+ALTER SEQUENCE public.competitions_id_seq RESTART WITH 1;
+ALTER SEQUENCE public.competitions_teams_id_seq RESTART WITH 1;
+ALTER SEQUENCE public.points_id_seq RESTART WITH 1;
+ALTER SEQUENCE public.presentations_id_seq RESTART WITH 1;
+ALTER SEQUENCE public.teams_id_seq RESTART WITH 1;
+ALTER SEQUENCE public.teams_users_id_seq RESTART WITH 1;
+ALTER SEQUENCE public.timeline_id_seq RESTART WITH 1;
+ALTER SEQUENCE public.users_id_seq RESTART WITH 1;
 
--- Tabela presentations
-INSERT INTO presentations (
-    "order", "entrance_choreography", "traditional_dance_1", "traditional_dance_2",
-    "traditional_dance_3", "exit_choreography", "birivas_dances_1", "birivas_dances_2",
-    "birivas_dances_3", "deleted", "created", "created_by", "updated", "updated_by"
-) VALUES
-    (0, 'GiraGira na vitrola', 'Anú', 'Tatu de castanholas', '', '', '', '', '', FALSE, NOW(), 3, NULL, NULL);
-
--- Tabela campus
-INSERT INTO campus (
-    "institution", "coordinator_name", "description", "entity", "dormitory", "id_presentation",
-    "deleted", "created", "created_by", "updated", "updated_by"
-) VALUES
-    ('IFFar - SVS', 'Eduardo Feitoza', 'Instituto Federal Farroupilha - Campus São Vicente do Sul', 'NTG Trempe da Saudade', 'Predio B', 1, FALSE, NOW(), 1, NULL, NULL);
-
-
-INSERT INTO users (
-    "name", "cpf", "registration", "password", "roles", "is_evaluator", "is_admin",
-    "id_campus", "deleted", "created", "created_by", "updated", "updated_by"
-) VALUES
-      ('Ana Beatriz Silva', '1', '1', '1', 'Professor', TRUE, FALSE, 1, FALSE, NOW(), 1, NULL, NULL),
-      ('Carlos Alberto Souza', '23456789012', '2023008910', '23456789012', 'Admin', FALSE, TRUE, 1, FALSE, NOW(), 1, NULL, NULL),
-      ('Fernanda Costa', '34567890123', '2023002345', '34567890123', 'Secretária', FALSE, FALSE, 1, FALSE, NOW(), 1, NULL, NULL),
-      ('Juliana Pereira', '45678901234', '2023006789', '45678901234', 'Estudante', TRUE, FALSE, 1, FALSE, NOW(), 1, NULL, NULL),
-      ('Roberto Lima', '56789012345', '2023001122', '56789012345', 'Professor', FALSE, FALSE, 1, FALSE, NOW(), 1, NULL, NULL),
-      ('Tatiane Mendes', '67890123456', '2023003344', '67890123456', 'Estudante', TRUE, TRUE, 1, FALSE, NOW(), 1, NULL, NULL);
-
--- Tabela users
-INSERT INTO users (
-    "name", "cpf", "registration", "password", "roles", "is_evaluator", "is_admin",
-    "id_campus", "deleted", "created", "created_by", "updated", "updated_by"
-) VALUES
-    ('Mauricio Carvalho Cogo', '05415375040', '2023001236', '05415375040', 'Estudante', FALSE, TRUE, 1, FALSE, NOW(), 1, NULL, NULL);
-
--- Tabela points
-INSERT INTO points (
-    "name", "type", "description", "icon", "latitude", "longitude",
-    "deleted", "created", "created_by", "updated", "updated_by"
-) VALUES
-      ('Dança de salão', 'Prova','NTG', 'trofeuC.png', '-29.704415687121692', '-54.69817036876705', FALSE, NOW(), 1, NULL, NULL),
-      ('Revezamento do mate', 'Prova', 'Garagem', 'trofeuC.png', '-29.704374820999202', '-54.69770526684055', FALSE, NOW(), 1, NULL, NULL),
-      ('Triatlo campeiro', 'Prova', 'Garagem','trofeuC.png', '-29.704334518267306', '-54.69768606687848', FALSE, NOW(), 1, NULL, NULL),
-      ('Tetarfe', 'Prova', 'Garagem', 'trofeuC.png', '-29.704296995027534', '-54.69766366693321', FALSE, NOW(), 1, NULL, NULL),
-      ('Tiro de laço vaca parada', 'Prova', 'Garagem', 'trofeuC.png', '-29.70425394454995', '-54.69764128484854', FALSE, NOW(), 1, NULL, NULL),
-      ('Pescaria gaúcha', 'Prova', 'Açude', 'trofeuC.png', '-29.70604223241599', '-54.69716708050202', FALSE, NOW(), 1, NULL, NULL),
-      ('Truco', 'Prova','NTG', 'trofeuC.png', '-29.704346952030676', '-54.69807047483297', FALSE, NOW(), 1, NULL, NULL),
-      ('Arroz Carreteiro', 'Prova', 'NTG', 'trofeuC.png', '-29.704470995951734', '-54.69804298867797', FALSE, NOW(), 1, NULL, NULL),
-      ('Artilharia gaúcha', 'Prova', 'Campo', 'trofeuC.png', '-29.702773175390206', '-54.695393463220356', FALSE, NOW(), 1, NULL, NULL),
-      ('Cabo de guerra', 'Prova','Campo', 'trofeuC.png', '-29.702455062590637', '-54.695073312707784', FALSE, NOW(), 1, NULL, NULL),
-      ('Bocha campeira', 'Prova', 'Campo', 'trofeuC.png', '-29.70212000404092', '-54.69482839582078', FALSE, NOW(), 1, NULL, NULL),
-      ('NTG', 'Local', 'Núcleo de Tradições Gaúchas Trempe da Saudade', 'alfinete.png', '-29.70419844938545', '-54.69796388301659', FALSE, NOW(), 1, NULL, NULL),
-      ('Refeitório', 'Local', 'Local de refeições, com menu saudável e mesas para interação social.', 'alfinete.png', '-29.703473889166226', '-54.69717865359698', FALSE, NOW(), 1, NULL, NULL),
-      ('Convivencia', 'Local', 'Local de convivencia dos estudantes', 'alfinete.png', '-29.702676486972212', '-54.696222011426585', FALSE, NOW(), 1, NULL, NULL),
-      ('CTG', 'Local', 'CTG Cancela da Fronteira', 'alfinete.png', '-29.685610487306235', '-54.676470859503844', FALSE, NOW(), 1, NULL, NULL),
-      ('Abertura', 'Local', 'Abertura Oficial do 30º Encontrão!', 'alfinete.png', '-29.693054480787954', '-54.6782872971689', FALSE, NOW(), 1, NULL, NULL),
-      ('IFFar-SVS', 'Local', 'IFFar São Vicente do Sul', 'instituto.png', '-29.702115796438793', '-54.696880623638656', FALSE, NOW(), 1, NULL, NULL),
-      ('Dormintórios', 'Local', 'Local para dormir, oferecendo um espaço tranquilo para relaxamento.', 'alfinete.png', '-29.693054480787954', '-54.6782872971689', FALSE, NOW(), 1, NULL, NULL);
-
--- Tabela competitions
-INSERT INTO competitions (
-    "name", "description", "modality", "fecult_event", "initial_date", "participants", "commission", "id_point",
-    "deleted", "created", "created_by", "updated", "updated_by"
-) VALUES
-      ('Dança de salão', 'Forme uma dupla, dance duas das seguintes danças: Chote, Milonga, Chamamé, Rancheira, Valsa, Marzuca, ou Bugio, em dois minutos cada, e conquiste a maior pontuação para vencer!', 'Padrão', FALSE, '2024-11-15 13:30:00', 2, TRUE, 1, FALSE, NOW(), 1, NULL, NULL),
-      ('Revezamento do mate', 'Forme uma equipe de quatro (dois peões e duas prendas), ceve, tome e passe o mate em revezamento. A equipe que completar a prova no menor tempo vence!', 'Padrão', FALSE, '2024-11-15 13:30:00', 4, FALSE, 2, FALSE, NOW(), 1, NULL, NULL),
-      ('Triatlo campeiro', 'Forme uma equipe de seis (três peões e três prendas) e complete as três etapas: nó de lenço, corrida do saco e debulhar milho. A equipe que finalizar todas as etapas primeiro vence!', 'Padrão', FALSE, '2024-11-15 14:30:00', 6, FALSE, 3, FALSE, NOW(), 1, NULL, NULL),
-      ('Tetarfe', 'Participe em duplas (um peão e uma prenda) e acumule pontos em lançamentos seguindo o Regulamento de Esportes Campeiros do MTG. Vence a equipe com a maior pontuação total. Em caso de empate, será considerado o maior desempenho em argola, ferradura e tava, nessa ordem.', 'Padrão', FALSE, '2024-11-15 13:30:00', 2, FALSE, 4, FALSE, NOW(), 1, NULL, NULL),
-      ('Tiro de laço vaca parada', 'Forme uma dupla, laçe as aspas da vaca parada a 8 metros (homens) ou 2 metros (mulheres) em três tentativas, e vença acertando mais laçadas válidas.', 'Masculino, Feminino e Patrão/Patroa', FALSE, '2024-11-16 08:00:00', 2, FALSE, 5, FALSE, NOW(), 1, NULL, NULL),
-      ('Pescaria gaúcha', 'Forme uma dupla com um peão e uma prenda, monte sua vara de pescar, busque sua isca e pesque o peixe mais pesado para vencer!', 'Padrão', FALSE, '2024-11-16 08:00:00', 2, FALSE, 6, FALSE, NOW(), 1, NULL, NULL),
-      ('Truco', 'Forme uma equipe de três e participe das partidas eliminatórias de Truco Gaudério. A competição segue o Regulamento de Esportes Campeiros do MTG, com formato “melhor de três partidas”. A equipe vencedora será a campeã!', 'Masculino e Feminino', FALSE, '2024-11-16 08:00:00', 3, FALSE, 7, FALSE, NOW(), 1, NULL, NULL),
-      ('Arroz Carreteiro', 'Forme uma equipe (um peão e uma prenda) e prepare arroz carreteiro para 40 pessoas. Os ingredientes e utensílios serão fornecidos, mas os temperos são de sua escolha para destacar sua identidade regional.', 'Padrão', FALSE, '2024-11-16 09:00:00', 2, TRUE, 8, FALSE, NOW(), 1, NULL, NULL),
-      ('Artilharia gaúcha', 'Forme uma equipe de quatro (dois peões e duas prendas) e arremesse pedras com um bodoque para atingir os alvos. Cada acerto vale um ponto e a equipe com mais pontos vence. Em caso de empate, cada participante terá mais um disparo. Traga seu bodoque tradicional!', 'Padrão', FALSE, '2024-11-16 13:30:00', 4, FALSE, 9, FALSE, NOW(), 1, NULL, NULL),
-      ('Cabo de guerra', 'Forme uma equipe de sete (mínimo de duas prendas) e participe do cabo de guerra com uma corda de 14 metros. Puxe a corda para seu lado e faça com que pelo menos um adversário ultrapasse a marca no chão. A equipe vencedora será a campeã!', 'Padrão', FALSE, '2024-11-16 14:30:00', 7, FALSE, 10, FALSE, NOW(), 1, NULL, NULL),
-      ('Bocha campeira', 'Forme uma equipe de três (dois peões e uma prenda) e jogue bocha campeira seguindo o Regulamento de Esportes Campeiros do MTG. As partidas são disputadas em duas passadas, com uma passada extra em caso de empate. A equipe com mais pontos vence!', 'Padrão', FALSE, '2024-11-16 13:30:00', 3, FALSE, 11, FALSE, NOW(), 1, NULL, NULL);
 
 -- Tabela teams
 INSERT INTO teams (
@@ -338,10 +276,6 @@ INSERT INTO timeline (
 
 
 
-\copy users (name, cpf, avatar, registration, password, roles, id_campus)
-    FROM 'C:/Users/Meu Computador/Documents/encontrão.csv'
-    DELIMITER ','
-    CSV HEADER;
 
 
 
@@ -349,6 +283,9 @@ INSERT INTO timeline (
 
 
 
+SELECT * FROM users u WHERE u.id_campus = 2 ORDER BY name;
+
+SELECT * FROM points p WHERE type = 'Local';
 
 
 
@@ -356,7 +293,55 @@ INSERT INTO timeline (
 
 
 
+SELECT * FROM teams;
 
+
+SELECT t.*
+FROM timeline t
+WHERE t.required = true;
+
+SELECT DISTINCT t.*, c.name AS competition_name
+FROM timeline t
+         JOIN competitions c ON t.id_competition = c.id
+         JOIN competitions_teams ct ON c.id = ct.id_competition
+         JOIN teams_users tu ON ct.id_team = tu.id_team
+WHERE tu.id_user = 1;
+
+SELECT DISTINCT c.id, c.name
+FROM competitions c
+         JOIN competitions_teams ct ON c.id = ct.id_competition
+         JOIN teams_users tu ON ct.id_team = tu.id_team
+WHERE tu.id_user = 1;
+
+SELECT
+    t.name,
+    t.type,
+    t.date,
+    NULL AS local  -- Adiciona uma coluna NULL para manter a mesma estrutura
+FROM
+    timeline t
+WHERE
+    t.required = true
+
+UNION ALL
+
+SELECT DISTINCT
+    t.name,
+    t.type,
+    t.date,
+    p.description AS local  -- Nome do ponto
+FROM
+    timeline t
+    JOIN
+    competitions c ON t.id_competition = c.id
+    JOIN
+    competitions_teams ct ON c.id = ct.id_competition
+    JOIN
+    teams_users tu ON ct.id_team = tu.id_team
+    JOIN
+    points p ON c.id_point = p.id   -- Juntando a tabela points para obter o local
+WHERE
+    tu.id_user = :userId;
 
 
 
